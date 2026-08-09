fbd7dced57906ba6f9f338e1bf9df69381af5f24
s0ulcrow <236109081+s0ulcrow@users.noreply.github.com>
2026-08-09 17:52:41 -0500

    production v1

--- main.js
+++ main.js
@@ -6,15 +6,22 @@ function createWindow() {
     height: 800,
     minWidth: 800,
     minHeight: 600,
+    show: false, // Don't show until ready
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true
     },
     title: 'AnythingGacha Studio',
-    autoHideMenuBar: true
+    autoHideMenuBar: true,
+    icon: __dirname + '/icon.ico' // Custom icon
   });
 
   win.loadFile('index.html');
+
+  // Show window once content is loaded (hides white flash)
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 app.whenReady().then(createWindow);
